c91086eff2cd5b63e2bdc930b8c1e19392e0ec34
Saif <mujawarmsaif@gmail.com>
2020-05-04 19:50:20 +0530

    first commit

---
+++ src/components/Copyright.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+export default class Copyright extends Component {
+	render() {
+		return (
+			<div>
+				<h4>IMPORTANT LINKS</h4>
+				<ul>
+					<li>
+						<a href="https://github.com/basir/ecommerce-shopping-cart">Download Source code </a>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
